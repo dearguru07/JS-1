7b83af2442b9555535d9a9735ba9d365aa6c2aa8
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-14 09:16:19 +0530

    'js'

---
+++ app.js
@@ -0,0 +1,31 @@
+// how to write h1 tag in vennila JS --------------- 
+// crateing the div and h1 and h2 tags using vennila JS-----------
+
+// let head=document.getElementById("body")
+// let parent=document.createElement("div")
+
+// let heading=document.createElement("h1")
+// let headingTwo=document.createElement("h2")
+
+// heading.innerHTML="helllo world...."
+// headingTwo.innerHTML="hi to all...."
+
+// head.appendChild(parent)
+// parent.appendChild(heading)
+// parent.appendChild(headingTwo)
+
+// >> this vennila Js craeting the DOM Elements----
+
+// console.log(name)
+
+// // var name=153
+// // let name="guru"
+
+
+
+let body=document.getElementById('body')
+let heading=document.createElement('h1')
+function Add(){
+    heading.innerHTML='hello world...'
+    heading.appendChild(head)
+}
